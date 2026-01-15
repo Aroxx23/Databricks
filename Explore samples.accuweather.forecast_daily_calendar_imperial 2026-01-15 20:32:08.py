0dcfa74f5d7376053ae457c602d890a103da48ea
Aroxx23 <arox2306@gmail.com>
2026-01-15 19:37:30 +0000

    Update

--- Explore samples.accuweather.forecast_daily_calendar_imperial 2026-01-15 20:32:08.py
+++ Explore samples.accuweather.forecast_daily_calendar_imperial 2026-01-15 20:32:08.py
@@ -1,3 +1,6 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC SELECT * FROM `samples`.`accuweather`.`forecast_daily_calendar_imperial`;
+# MAGIC SELECT date, temperature_max, temperature_min, precipitation_probability
+# MAGIC FROM `samples`.`accuweather`.`forecast_daily_calendar_imperial`
+# MAGIC ORDER BY date DESC
+# MAGIC LIMIT 10;
